14c9e72fef452d74af22f62a4b9ce90dfba4cc3f
Noel Perez <erick.perez1927@gmail.com>
2024-04-11 01:47:33 -0600

    initial commit

---
+++ src/config/fonts.ts
@@ -0,0 +1,8 @@
+import { JetBrains_Mono } from "next/font/google";
+
+export const fontBase = JetBrains_Mono({
+  weight: ["100", "800"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
+});
